ecbf975bab333b50514faf09e4322d9e4cba7870
Ramone Clarke <ramonejlclarke@gmail.com>
2023-04-12 15:49:23 +0100

    fix import error

--- pages/designs/snippet-view.jsx
+++ pages/designs/snippet-view.jsx
@@ -2,6 +2,12 @@ import Frame from "@/components/design/Frame";
 import SnippetViewDesign from "@/components/design/SnippetViewDesign";
 import Head from "next/head";
 import React, { useState } from "react";
+import {
+  FaHome,
+  FaListAlt,
+  FaRegHandScissors,
+  FaSearchengin,
+} from "react-icons/fa";
 
 const SnippetViewPage = () => {
   const [darkMode, setDarkMode] = useState(false);
@@ -26,5 +32,24 @@ const SnippetViewPage = () => {
     </>
   );
 };
+
+const nav = [
+  {
+    text: "Home",
+    icon: <FaHome size="1.3rem" className="text-brand" />,
+  },
+  {
+    text: "Snippets",
+    icon: <FaRegHandScissors size="1.3rem" className="text-brand" />,
+  },
+  {
+    text: "Summaries",
+    icon: <FaListAlt size="1.3rem" className="text-brand" />,
+  },
+  {
+    text: "Search",
+    icon: <FaSearchengin size="1.3rem" className="text-brand" />,
+  },
+];
 
 export default SnippetViewPage;

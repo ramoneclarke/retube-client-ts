ca7ae8188cbabdddc69b3e2fcf9fd06f0b93b37d
Ramone Clarke <ramonejlclarke@gmail.com>
2023-04-16 16:35:28 +0100

    remove csrf token request

--- pages/index.js
+++ pages/index.js
@@ -22,22 +22,22 @@ export default function Home() {
 
   const { data: session, status, update } = useRefetchingSession();
 
-  useEffect(() => {
-    const csrftoken = Cookies.get("csrftoken");
-    if (csrftoken === undefined) {
-      fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_BASE}/api/csrf/`, {
-        credentials: "include",
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("data: ", data);
-          Cookies.set("csrftoken", data["X-CSRFToken"]);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  }, []);
+  // useEffect(() => {
+  //   const csrftoken = Cookies.get("csrftoken");
+  //   if (csrftoken === undefined) {
+  //     fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_BASE}/api/csrf/`, {
+  //       credentials: "include",
+  //     })
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         console.log("data: ", data);
+  //         Cookies.set("csrftoken", data["X-CSRFToken"]);
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //       });
+  //   }
+  // }, []);
 
   const handleSignOut = async () => {
     try {
@@ -48,7 +48,6 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
           "X-CSRFToken": csrftoken,
-          Authorization: `Bearer ${session.accessToken}`,
         },
         credentials: "include",
       }).then((response) => {

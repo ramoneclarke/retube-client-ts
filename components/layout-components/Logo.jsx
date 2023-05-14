f7b7a65efcfc540442c89619368471bed9b53c5e
Ramone Clarke <ramonejlclarke@gmail.com>
2023-05-14 14:00:49 +0100

    fix sidebar logo bg color

--- components/layout-components/Logo.jsx
+++ components/layout-components/Logo.jsx
@@ -3,12 +3,13 @@ import React from "react";
 
 const Logo = () => {
   return (
-    <Link href="/" className="">
-      <div className="flex h-full w-fit flex-row items-center bg-lightest pl-4 dark:bg-darkest lg:w-1/5 lg:min-w-[16rem] lg:pl-8 ">
-        <div className="text-3xl font-extrabold">
-          <span className="text-darkest dark:text-lightest">Re</span>
-          <span className="text-brand">Tube</span>
-        </div>
+    <Link
+      href="/"
+      className="flex h-full w-fit flex-row items-center bg-lightest pl-4 dark:bg-darkest lg:w-1/5 lg:min-w-[16rem] lg:pl-8 "
+    >
+      <div className="text-3xl font-extrabold">
+        <span className="text-darkest dark:text-lightest">Re</span>
+        <span className="text-brand">Tube</span>
       </div>
     </Link>
   );

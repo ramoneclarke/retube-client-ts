403368306ce4e6a53ed50e717e385591c786706d
Ramone Clarke <ramonejlclarke@gmail.com>
2023-08-07 17:28:45 +0100

    refactor LinkActionButton

--- components/Shared/LinkActionButton.jsx
+++ components/Shared/LinkActionButton.jsx
@@ -4,8 +4,7 @@ import React from "react";
 const LinkActionButton = ({ text, handleLoadVideo, inputText, summarise }) => {
   return (
     <motion.button
-      //   type="submit"
-      className="no-tap-highlight flex h-full w-1/6 items-center justify-center rounded-lg bg-violet-600 font-medium text-lightest shadow-md dark:bg-violet-600"
+      className="no-tap-highlight flex h-full min-h-[4rem] w-1/6 max-w-[10rem] items-center justify-center rounded-lg bg-violet-600 font-medium text-lightest shadow-md dark:bg-violet-600"
       onClick={() => handleLoadVideo(inputText)}
       whileTap={{
         scale: 0.9,

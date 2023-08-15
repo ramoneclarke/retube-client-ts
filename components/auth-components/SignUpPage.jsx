17d23024b4bd92d6235e79ab1ccbb6fca6b8d444
Ramone Clarke <ramonejlclarke@gmail.com>
2023-08-15 14:35:05 +0100

    change styles of login and signup pages for small screens

--- components/auth-components/SignUpPage.jsx
+++ components/auth-components/SignUpPage.jsx
@@ -9,8 +9,8 @@ const SignUpPage = () => {
       <div className="absolute top-4 left-4">
         <Logo />
       </div>
-      <div className="flex h-96 w-2/5 min-w-[10rem] flex-col items-center gap-2 p-4 pt-8">
-        <h1 className="mb-8 text-5xl font-semibold text-darkest dark:text-lightest">
+      <div className="flex h-fit w-full min-w-[10rem] flex-col items-center gap-2 p-4 pt-8 md:h-96 lg:w-2/5">
+        <h1 className="mb-2 text-3xl font-semibold text-darkest dark:text-lightest md:mb-8 md:text-5xl">
           Create your account
         </h1>
         <SignUpWithGoogleButton />

06d41f43d17a8f81cf5b1071bfde1fa021574bce
Ramone Clarke <ramonejlclarke@gmail.com>
2023-08-24 19:24:57 +0100

    convert color mode context from to typescript

---
+++ context/ColorModeContext.tsx
@@ -0,0 +1,51 @@
+import React, {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+interface ColorModeContext {
+  darkMode: boolean;
+  toggleDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const ColorModeContext = createContext<ColorModeContext>({
+  darkMode: true,
+  toggleDarkMode: () => {},
+});
+
+interface ColorModeProviderProps {
+  children: ReactNode;
+}
+
+export const ColorModeProvider: React.FC<ColorModeProviderProps> = ({
+  children,
+}) => {
+  const [darkMode, setDarkMode] = useState(true);
+
+  const toggleDarkMode = () => {
+    const nextColorMode = darkMode ? "light" : "dark";
+    setDarkMode(!darkMode);
+    window.localStorage.setItem("colorMode", nextColorMode);
+  };
+
+  useEffect(() => {
+    const localColorMode = window.localStorage.getItem("colorMode");
+    if (localColorMode) {
+      const nextMode = localColorMode === "light" ? false : true;
+      setDarkMode(nextMode);
+    }
+  }, []);
+
+  return (
+    <ColorModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </ColorModeContext.Provider>
+  );
+};
+
+export const useColorMode = () => {
+  return useContext(ColorModeContext);
+};
